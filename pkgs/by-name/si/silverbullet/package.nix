e759079479de924f876a8570d49d0507a41b7fd1
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2026-01-26 16:18:15 +0000

    fix: silverbullet packaging

--- pkgs/by-name/si/silverbullet/package.nix
+++ pkgs/by-name/si/silverbullet/package.nix
@@ -1,62 +1,37 @@
 {
-  autoPatchelfHook,
-  common-updater-scripts,
-  fetchzip,
+  buildGoModule,
+  deno,
+  fetchFromGitHub,
   lib,
-  nixosTests,
+  git,
   stdenv,
-  stdenvNoCC,
-  writeShellScript,
 }:
-stdenvNoCC.mkDerivation (finalAttrs: {
+
+buildGoModule rec {
   pname = "silverbullet";
   version = "2.4.1";
 
-  src =
-    finalAttrs.passthru.sources.${stdenv.hostPlatform.system}
-      or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+  src = fetchFromGitHub {
+    owner = "silverbulletmd";
+    repo = "silverbullet";
+    rev = version;
+    hash = "sha256-AjaD3KwHYHj+vkW8E2jr9BjhP+HCWSMwKklSNmAuaLE=";
+  };
 
-  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
+  vendorHash = "sha256-R/jjUWLCAmhSJUSi91+iWxHqRcxq6mpz5rvz+5PFV2A=";
 
-  buildInputs = [ stdenv.cc.cc.lib ];
+  nativeBuildInputs = [
+    deno
+    git
+  ];
 
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp $src/silverbullet $out/bin/
-    runHook postInstall
+  preBuild = ''
+    export HOME=$TMPDIR
+    export DENO_DIR=$TMPDIR/.deno
+
+    # Build the frontend with deno (production build)
+    deno task build-production
   '';
-
-  passthru = {
-    sources = {
-      "x86_64-linux" = fetchzip {
-        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-linux-x86_64.zip";
-        hash = "sha256-1jUN22T7BABBEiXp1LwMUaw/ELR7CZS2iKLaoiYKeLk=";
-        stripRoot = false;
-      };
-      "aarch64-linux" = fetchzip {
-        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-linux-aarch64.zip";
-        hash = "sha256-F0CTVbVK2xaqNlHX1If8EhlDRqb+XIZlWmzkEYFGS3M=";
-        stripRoot = false;
-      };
-      "x86_64-darwin" = fetchzip {
-        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-darwin-x86_64.zip";
-        hash = "sha256-lWUTNGt5u7q5cs7xoNP/k7GYUq/A3xHI6rza8HYOK5Y=";
-        stripRoot = false;
-      };
-      "aarch64-darwin" = fetchzip {
-        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-darwin-aarch64.zip";
-        hash = "sha256-1lPGipv6jW0Awjy7V9HORK5oh5RDpBBrT4zZk0oSVWY=";
-        stripRoot = false;
-      };
-    };
-
-    updateScript = writeShellScript "update-silverbullet" ''
-      NEW_VERSION="$1"
-      for platform in ${lib.escapeShellArgs finalAttrs.meta.platforms}; do
-        ${lib.getExe' common-updater-scripts "update-source-version"} "silverbullet" "$NEW_VERSION" --ignore-same-version --source-key="sources.$platform"
-      done
-    '';
 
     tests = {
       inherit (nixosTests) silverbullet;
@@ -64,12 +39,11 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   };
 
   meta = {
-    changelog = "https://github.com/silverbulletmd/silverbullet/blob/${finalAttrs.version}/website/CHANGELOG.md";
+    changelog = "https://github.com/silverbulletmd/silverbullet/blob/${version}/CHANGELOG.md";
     description = "Open-source, self-hosted, offline-capable Personal Knowledge Management (PKM) web application";
     homepage = "https://silverbullet.md";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ aorith ];
     mainProgram = "silverbullet";
-    platforms = builtins.attrNames finalAttrs.passthru.sources;
   };
-})
+}
